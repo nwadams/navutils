17f2099cec8df2c6e36474b0d13fd2cefa9e51a4
Nick Adams <nwadams@nickadams.ca>
2015-05-11 21:32:06 -0700

    add up intent navigation.

---
+++ app/src/main/java/ca/nickadams/navutils/ChildActivity.java
@@ -0,0 +1,40 @@
+package ca.nickadams.navutils;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ChildActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        Log.d(ChildActivity.class.getSimpleName(), "starting activity");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), ChildActivity.class));
+                finish();
+            }
+        });
+    }
+
+    @Nullable
+    @Override
+    public Intent getSupportParentActivityIntent() {
+        Intent intent = super.getSupportParentActivityIntent();
+        if (intent != null) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        }
+        return intent;
+    }
+}
--- app/src/main/java/ca/nickadams/navutils/MainActivity.java
+++ app/src/main/java/ca/nickadams/navutils/MainActivity.java
@@ -1,17 +1,31 @@
 package ca.nickadams.navutils;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), ChildActivity.class));
+            }
+        });
+
+        Log.d(MainActivity.class.getSimpleName(), "starting activity");
     }
 
     @Override
